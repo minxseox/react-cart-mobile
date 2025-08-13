a7aa118cbd746483da69f787efa432ace2cfb18f
minseo <gimminseo288@gamil.com>
2025-08-13 17:17:19 +0900

    Fix GH Pages: set base and router

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// 개발: '/', 배포: '/react-cart-mobile/'
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/react-cart-mobile/" : "/",
+  base: "/react-cart-mobile/", // ← 리포지토리명으로 꼭 설정
 });
