ecbbb60a269441cbb440293c7450c4ef8e21712e
minseo <gimminseo288@gamil.com>
2025-08-13 23:00:00 +0900

    img

--- src/data/products.js
+++ src/data/products.js
@@ -1,46 +1,49 @@
-const products = [
-  {
-    id: 1,
-    brand: "브랜드A",
-    description: "편안하고 착용감이 좋은 신발",
-    price: 35000,
-    image: "/images/1.jpg",
-  },
-  {
-    id: 2,
-    brand: "브랜드A",
-    description: "힙한 컬러가 매력적인 신발",
-    price: 25000,
-    image: "/images/2.jpg",
-  },
-  {
-    id: 3,
-    brand: "브랜드B",
-    description: "편안하고 착용감이 좋은 신발",
-    price: 35000,
-    image: "/images/3.jpg",
-  },
-  {
-    id: 4,
-    brand: "브랜드B",
-    description: "힙한 컬러가 매력적인 신발",
-    price: 35000,
-    image: "/images/4.jpg",
-  },
-  {
-    id: 5,
-    brand: "브랜드C",
-    description: "편안하고 착용감이 좋은 신발",
-    price: 35000,
-    image: "/images/5.jpg",
-  },
-  {
-    id: 6,
-    brand: "브랜드C",
-    description: "힙한 컬러가 매력적인 신발",
-    price: 35000,
-    image: "/images/6.jpg",
-  },
-];
+// 로컬 이미지 파일명만 사용합니다. (public/images/<파일명>)
+const products = {
+  photo: [
+    {
+      id: 1,
+      name: "브랜드A",
+      info: "편안하고 착용감이 좋은 신발",
+      price: "35,000원",
+      img: "1.jpg",
+    },
+    {
+      id: 2,
+      name: "브랜드A",
+      info: "힙한 컬러가 매력적인 신발",
+      price: "25,000원",
+      img: "2.jpg",
+    },
+    {
+      id: 3,
+      name: "브랜드B",
+      info: "편안하고 착용감이 좋은 신발",
+      price: "35,000원",
+      img: "3.jpg",
+    },
+    {
+      id: 4,
+      name: "브랜드B",
+      info: "힙한 컬러가 매력적인 신발",
+      price: "35,000원",
+      img: "4.jpg",
+    },
+    {
+      id: 5,
+      name: "브랜드C",
+      info: "편안하고 착용감이 좋은 신발",
+      price: "35,000원",
+      img: "5.jpg",
+    },
+    {
+      id: 6,
+      name: "브랜드C",
+      info: "힙한 컬러가 매력적인 신발",
+      price: "35,000원",
+      img: "6.jpg",
+    },
+  ],
+};
 
 export default products;
