3fadd30491cd958f6935e984833b7fa03cee5600
minseo <gimminseo288@gamil.com>
2025-08-13 22:32:53 +0900

    add jpg

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-cart-mobile/", // ← 리포지토리명으로 꼭 설정
+  base: "/react-cart-mobile/", // ✅ 반드시 추가
 });
